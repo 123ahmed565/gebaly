50a48d15711ef1f0bd27e5730f37566ecab7638b
123ahmed565 <ahmedseragmohamed@gmail.com>
2021-10-27 11:26:03 +0200

    add sections (testimonials brands latest articles rtl ltr responsive)

--- js/main.js
+++ js/main.js
@@ -32,3 +32,50 @@ var swiper = new Swiper(".mySwiper", {
 
   // navtabs why choose us
   
+
+  // testimonials
+  var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    freeMode: true,
+    grabCursor:true,
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+// brands
+  var swiper = new Swiper(".mySwiper3", {
+    slidesPerView: 6.5,
+    spaceBetween: 30,
+    freeMode: true,
+    grabCursor:true,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+      0: {
+        slidesPerView: 3.5,
+        spaceBetween: 15
+    
+      },
+      600: {
+        slidesPerView: 4.5,
+        spaceBetween: 20
+      },
+      1000: {
+        slidesPerView: 6.5,
+        spaceBetween: 20
+      },
+        
+    }
+  });
