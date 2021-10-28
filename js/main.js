06c1c9ca53207ef7efd62ed3b28b68a039c56348
123ahmed565 <ahmedseragmohamed@gmail.com>
2021-10-28 17:38:25 +0200

    just for check update

--- js/main.js
+++ js/main.js
@@ -110,10 +110,20 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
+// nav tab vertically about us
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
-// pop up for modal appointment
-//   function modalToggle(){
-//     const modal = document.getElementById('modal');
-//     modal.classList.toggle('active');
-// }
-
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
