8f0bcce24faad37aee8bdbac59ea4aeabbaf8c08
123ahmed565 <ahmedseragmohamed@gmail.com>
2021-11-09 17:55:25 +0200

    edite in all website

--- js/main.js
+++ js/main.js
@@ -111,24 +111,24 @@ var swiper = new Swiper(".mySwiper", {
   // };
 
 // statistics laod number when scroll
-const counters = document.querySelectorAll('.counter');
-window.addEventListener('scroll',()=>{
-  counters.forEach(counter => {
-    counter.innerText = '0';
+// const counters = document.querySelectorAll('.counter');
+// window.addEventListener('scroll',()=>{
+//   counters.forEach(counter => {
+//     counter.innerText = '0';
     
-    const updateCounter = ()=>{
-      const target=counter.getAttribute('data-target');
-      const c = +counter.innerText;
+//     const updateCounter = ()=>{
+//       const target=counter.getAttribute('data-target');
+//       const c = +counter.innerText;
     
-      const increment =  target /500;
-      if(c <target){
-        counter.innerText=  `${Math.ceil(c+increment)}`;
-        setTimeout(updateCounter, 1);
-      }else{
-        counter.innerText=target;
-      }
-    } ;
-    updateCounter();
-    });
-})
+//       const increment =  target /500;
+//       if(c <target){
+//         counter.innerText=  `${Math.ceil(c+increment)}`;
+//         setTimeout(updateCounter, 1);
+//       }else{
+//         counter.innerText=target;
+//       }
+//     } ;
+//     updateCounter();
+//     });
+// })
 
