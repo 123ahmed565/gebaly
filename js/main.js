111562fa156f9125daebbddeecd742ca67289cbf
123ahmed565 <ahmedseragmohamed@gmail.com>
2021-11-10 11:34:40 +0200

    edit in form modal popup

--- js/main.js
+++ js/main.js
@@ -132,3 +132,15 @@ var swiper = new Swiper(".mySwiper", {
 //     });
 // })
 
+
+// Put this script in header or above select element
+function check(elem) {
+  // use one of possible conditions
+  // if (elem.value == 'Other')
+  if (elem.selectedIndex == 1) {
+      document.getElementById("other-div").style.display = 'block';
+  } else {
+      document.getElementById("other-div").style.display = 'none';
+  }
+}
+
